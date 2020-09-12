8f419e86d0d8d675789a706fbf9aa5bce128015e
Bonnie Ward <bonniesbrightideas@gmail.com>
2020-09-11 18:35:56 -0700

    pop up

--- js/index.js
+++ js/index.js
@@ -2,3 +2,16 @@
     $(".preloader").fadeOut("slow");
 }
 setTimeout(fade, 2000); */
+
+/* Tool Tip  line 116  */
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  }) 
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
